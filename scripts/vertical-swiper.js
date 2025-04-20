9085e8e94d5673e41fee790c3b9467eb6f6d3655
Iryna B <114106094+Iryna-Bigdash@users.noreply.github.com>
2025-04-20 22:21:41 +0100

    improved seo

--- scripts/vertical-swiper.js
+++ scripts/vertical-swiper.js
@@ -1,29 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const swiper = new Swiper('.swiper', {
-//       direction: 'vertical',
-//       loop: true,
-//       autoplay: {
-//         delay: 4000,
-//         disableOnInteraction: false,
-//       },
-//       pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//       },
-//       slidesPerView: 1,
-//       spaceBetween: 20,
-//       breakpoints: {
-//         768: {
-//           slidesPerView: 1,
-//         },
-//         1024: {
-//           slidesPerView: 1,
-//         },
-//       },
-//     });
-//   });
 document.addEventListener('DOMContentLoaded', function () {
-  // Горизонтальний swiper для відгуків
   new Swiper('.reviews-swiper', {
     loop: true,
     autoplay: {
